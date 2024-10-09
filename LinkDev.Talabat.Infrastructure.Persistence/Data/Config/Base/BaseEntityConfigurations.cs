2c948303956ed173a1a2daa65158fe5fa53b36c9
Amr Mohamed Fathallah <amrmfathallah11@gmail.com>
2024-10-09 20:09:33 +0300

    {Update} Part 01 BaseEntity and BaseAuditableEntity

--- LinkDev.Talabat.Infrastructure.Persistence/Data/Config/Base/BaseEntityConfigurations.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/Data/Config/Base/BaseEntityConfigurations.cs
@@ -9,27 +9,12 @@ using System.Threading.Tasks;
 namespace LinkDev.Talabat.Infrastructure.Persistence.Data.Config.Base
 {
 	internal class BaseEntityConfigurations<TEntity, TKey> : IEntityTypeConfiguration<TEntity>
-		where TEntity : BaseAuditableEntity<TKey> where TKey : IEquatable<TKey>
+		where TEntity : BaseEntity<TKey> where TKey : IEquatable<TKey>
 	{
 		public virtual void Configure(EntityTypeBuilder<TEntity> builder)
 		{
 			builder.Property(E => E.Id)
 				.ValueGeneratedOnAdd();
-
-			builder.Property(E => E.CreatedBy)
-				.IsRequired();
-
-			builder.Property(E => E.CreatedOn)
-				.IsRequired()
-				/*.HasDefaultValueSql("GETUTCDate()")*/;
-
-			builder.Property(E => E.LastModifiedBy)
-				.IsRequired();
-
-			builder.Property(E => E.LastModifiedOn)
-				.IsRequired()
-				/*.HasDefaultValueSql("GETUTCDate()")*/;
 		}
 	}
-
 }
