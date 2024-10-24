f0d9c6b98e23bf4f3e8d5d7cc6416e37e4e1b916
Amr Mohamed Fathallah <amrmfathallah11@gmail.com>
2024-10-25 00:10:32 +0300

    {Update} Part 01 Identity Module - Entities

--- LinkDev.Talabat.Core.Domain/Entities/Identity/Address.cs
+++ LinkDev.Talabat.Core.Domain/Entities/Identity/Address.cs
@@ -15,6 +15,7 @@ namespace LinkDev.Talabat.Core.Domain.Entities.Identity
         public required string City { get; set; }
         public required string Country { get; set; }
 
+
         public required string UserId { get; set; }
 
         public virtual required ApplicationUser User { get; set; }
