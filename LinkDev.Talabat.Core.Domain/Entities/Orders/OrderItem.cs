961e39861d7f24abe25c5f17f6fb590f2df2bbc1
Amr Mohamed Fathallah <amrmfathallah11@gmail.com>
2024-11-24 01:52:25 +0200

    Part 03 Order Module - Migrations and Data Seeding

--- LinkDev.Talabat.Core.Domain/Entities/Orders/OrderItem.cs
+++ LinkDev.Talabat.Core.Domain/Entities/Orders/OrderItem.cs
@@ -7,6 +7,18 @@ namespace LinkDev.Talabat.Core.Domain.Entities.Orders
 {
 	public class OrderItem : BaseAuditableEntity<int>
 	{
+
+		public OrderItem()
+		{
+		}
+		public OrderItem(ProductItemOrdered product, decimal price, int quatity)
+		{
+			Product = product;
+			Price = price;
+			Quatity = quatity;
+		}
+
+
 		public required ProductItemOrdered Product { get; set; }
 		public decimal Price { get; set; }
 		public int Quatity { get; set; }
--- LinkDev.Talabat.Infrastructure.Persistence/_Data/StoreDbContext.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/_Data/StoreDbContext.cs
@@ -1,5 +1,6 @@
 ﻿
 using LinkDev.Talabat.Core.Domain.Common;
+using LinkDev.Talabat.Core.Domain.Entities.Orders;
 using LinkDev.Talabat.Core.Domain.Entities.Products;
 using LinkDev.Talabat.Infrastructure.Persistence._Common;
 using System.Reflection;
@@ -9,12 +10,16 @@ namespace LinkDev.Talabat.Infrastructure.Persistence.Data
 	public class StoreDbContext : DbContext
 	{
 		public DbSet<Product> Products { get; set; }
-        public DbSet<ProductBrand> Brands { get; set; }
-        public DbSet<ProductCategory> Categories { get; set; }
-        public StoreDbContext(DbContextOptions<StoreDbContext> options): base(options)
-        {
-            
-        }
+		public DbSet<ProductBrand> Brands { get; set; }
+		public DbSet<ProductCategory> Categories { get; set; }
+
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderItem> OrderItems { get; set; }
+		public DbSet<DeliveryMethod> DeliveryMethods{ get; set; }
+		public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+		{
+
+		}
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
@@ -23,7 +28,7 @@ namespace LinkDev.Talabat.Infrastructure.Persistence.Data
 				type => type.GetCustomAttribute<DbContextTypeAttribute>()?.DbContextType == typeof(StoreDbContext));
 		}
 
-		
+
 
 	}
 }
