b10272dfae90535f969a49ba8df3da12ef8c7eb3
Amr Mohamed Fathallah <amrmfathallah11@gmail.com>
2024-11-24 15:15:16 +0200

    Part 08 GetOrdersForUser, GetOrderForUser, GetDeliveryMethods endpoints

--- LinkDev.Talabat.Core.Domain/Common/BaseAuditableEntity.cs
+++ LinkDev.Talabat.Core.Domain/Common/BaseAuditableEntity.cs
@@ -9,9 +9,9 @@ namespace LinkDev.Talabat.Core.Domain.Common
 	public abstract class BaseAuditableEntity<TKey> : BaseEntity<TKey>
         where TKey : IEquatable<TKey>
 	{
-        public string CreatedBy { get; set; } = null!;
-        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
-        public string LastModifiedBy { get; set; } = null!;
-        public DateTime LastModifiedOn { get; set; } = DateTime.UtcNow;
+        public string? CreatedBy { get; set; } = null!;
+        public DateTime? CreatedOn { get; set; } = DateTime.UtcNow;
+        public string? LastModifiedBy { get; set; } = null!;
+        public DateTime? LastModifiedOn { get; set; } = DateTime.UtcNow;
     }
 }
